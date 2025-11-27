1116002a06260ee87cc946e4d268d8f5e2ebd479
wangyongliang <viporg@126.com>
2025-11-28 01:17:25 +0800

    Update Package.swift for v20251128

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
         .binaryTarget(
             name: "OnnxRuntime",
             url: "https://github.com/fdddf/VoiceNotesBinary/releases/download/v20251128/onnxruntime.xcframework.zip",
-            checksum: "dcc7fc35332e643be602fdf63c6b746e2ce39e73eae2accb372c7e5a1379339a"
+            checksum: "ef1fc9580f96ffc3a6a8d8c1763d22efd7ac84628747c4a65ecbfe0813489656"
         ),
         .binaryTarget(
             name: "SherpaOnnx",
             url: "https://github.com/fdddf/VoiceNotesBinary/releases/download/v20251128/sherpa-onnx.xcframework.zip",
-            checksum: "bbd1b422ef7b9e8690a0dd04373b94cc3349411510e2e79d6350e9b4674777b3"
+            checksum: "7eac1bf974162cc674d3c9d5d5be6619cd3ba9e4930d82e0c058ff4433be99f7"
         ),
     ]
 )
